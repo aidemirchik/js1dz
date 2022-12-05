7bf06ad31ebac4812646e8cfe056f1be6b05ffcc
aidemirchik <awirumy14@gmail.com>
2022-12-05 16:39:19 +0600

    js1dz

---
+++ светофор/main.js
@@ -0,0 +1,23 @@
+let color = prompt('ВВЕДИТЕ ЦВЕТ',);
+// alert(`stop`);
+
+// let color2 = prompt('ВВЕДИТЕ ЦВЕТ');
+// alert(`wite`);
+
+// let color3 = prompt('ВВЕДИТЕ ЦВЕТ',);
+// alert(`go`);
+
+    switch (color) {
+        case 'red':
+            alert('stop');
+            break;
+            case 'yellow':
+            alert('wait');
+            break;
+    
+            case 'green':
+            alert('go');
+            break;
+        default:
+            alert('unknown color');
+    }
---
+++ числа сравнение/main.js
@@ -0,0 +1,10 @@
+let num1 = prompt("Введите число")
+let num2 = prompt("Введите число")
+
+if (num1 > num2) {
+    alert(num1 + " больше чем " + num2)
+}else if (num1 < num2) {
+    alert(num1 + " меньше чем " + num2)
+}else {
+    alert(" равны ")
+}
